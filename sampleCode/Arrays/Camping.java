d5db6d3f2bcbe6a89aaddd071b0a8e9086d14e56
Darwin Schweitzer <darsch@microsoft.com>
2022-01-10 00:01:11 +0000

    camping

---
+++ sampleCode/Arrays/Camping.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+
+public class Camping {
+    public static void main(String[] args) {
+    
+      // dogNames array here
+      
+      String[] dogNames = new String[3];
+      dogNames[0]="Baby";
+      dogNames[1]="Harlow";
+      dogNames[2]="Hudson";
+
+
+      
+      // babyGrowls array here and call countBabyGrowls Method
+
+
+
+       
+      // harlowNaps array here and call increaseNap Method as well as minHarlowNaps and maxHarlowNaps Methods
+      
+
+
+      
+      // holdHudson array here and call sumHudsonHold Method
+      
+      System.out.println(Arrays.toString(dogNames));
+      // Println statements here
+      /* Should output
+      Baby growled 43 times!
+      Harlow napped Minimum: 0.5 Maximum: 3.25
+      Hudson was held 6 times!
+      */
+
+      
+
+
+    } //end of main method
+    
+    // countBabyGrowls static method here
+    
+
+
+
+
+
+
+    
+    // increaseNap static method here
+    
+
+
+
+    
+    // minHarlowNaps static method here     
+    
+
+
+
+
+
+
+
+    
+    // maxHarlowNaps static method here    
+    
+
+
+
+    
+    
+    // sumHudsonHold static method here  
+    
+
+
+
+
+
+    
+}
