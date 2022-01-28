6c1a8458779c481968cf1049db36109e59ce7ac0
Darwin Schweitzer <darsch@microsoft.com>
2022-01-28 04:34:06 +0000

    shopping

---
+++ sampleCode/ArrayList/ShoppingList.java
@@ -0,0 +1,33 @@
+package sampleCode.ArrayList;
+
+import java.util.ArrayList;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+
+        ArrayList <String> shoppingList = new ArrayList<String>();
+
+        //String box = "Wheaties";
+
+        //shoppingList.add(box);
+
+        //shoppingList.add("bananas");
+
+        //shoppingList.set(1, "Bananas");
+
+        //shoppingList.remove(0);
+
+        //shoppingList.add("Eggs");
+
+        //shoppingList.clear();
+
+        System.out.println(shoppingList);
+        System.out.println("There are " + shoppingList.size() + " in the list");
+
+        //System.out.println(shoppingList.toString());
+
+    }
+
+
+}
