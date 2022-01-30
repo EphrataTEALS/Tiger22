5b5b6fb1fe296c8f49a0cb4325265b0bbd2e36ff
Jim Andress <jandress@lirio.co>
2022-01-30 12:11:01 -0800

    add wordle starter code

---
+++ sampleCode/Wordle/WordLists.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+//////////////////////////////////////////////////////////////////////
+/*                                                                  */
+/*                                                                  */
+/*                                                                  */
+/*                                                                  */
+/*                                                                  */
+/*                     DO NOT MODIFY THIS FILE                      */
+/*                                                                  */
+/*                                                                  */
+/*                                                                  */
+/*                                                                  */
+/*                                                                  */
+//////////////////////////////////////////////////////////////////////
+
+public class WordLists {
+   public static final ArrayList<String> WORDS = new ArrayList<String>();
+   public static final ArrayList<String> TARGETS = new ArrayList<String>();
+   
+   public static void loadFile(ArrayList<String> list, String filepath) {
+      BufferedReader reader;
+      try {
+         reader = new BufferedReader(new FileReader(filepath));
+         String line = reader.readLine();
+         while (line != null) {
+            list.add(line);
+            line = reader.readLine();
+         }
+         reader.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      Collections.sort(list);
+   }
+   
+   static {
+      loadFile(WORDS, "words.txt");
+      loadFile(TARGETS, "targets.txt");
+   }
+}
