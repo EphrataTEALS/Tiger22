fca31e121581afbeb37ea648b49c8b4632b62ec8
ankurdhoot <ankurdhoot97@gmail.com>
2022-02-17 16:19:30 +0000

    Fix merge commit

--- sampleCode/ArrayTraversal/TraversalTests.java
+++ sampleCode/ArrayTraversal/TraversalTests.java
@@ -1,16 +1,14 @@
 package sampleCode.ArrayTraversal;
-
 public class TraversalTests {
    public static void main(String[] args) {
       boolean part1 = false;
       boolean part2 = false;
       boolean part3 = false;
-      boolean part4 = true;
-      
+      boolean part4 = false;
+
       if (part1) {
          runPart1(); // put your code in the printArray() method
       }
-
       if (part2) {
          runPart2(); // put your code in the areArraysEqual() method
       }
@@ -29,51 +27,31 @@ public class TraversalTests {
        * This method should print the contents of an array on a single line. 
        * It should work for arrays with no elements, one element, or more than one element.
        *
-       * Here are some formatting examples:
-       *    - if the array has no elements, it should print "[]"
-       *    - if the array had a single element 1, it should print "[1]"
-       *    - if the array had three elements 1, 23, and 456, it should print "[1, 23, 456]"
+	@@ -36,7 +36,15 @@
        */
-       
+
       // Your code goes below this line (suggested length: ~10 lines)
-      if (arr.length == 0) {
-         System.out.println("[]");
-      } else {
-         System.out.print("[" + arr[0]);
-         for (int i = 1; i < arr.length; i++) {
-            System.out.print("," + arr[i]);
-         }
-         System.out.println("]");
-      }    
+      System.out.println(arr);      
    }
-   
+
    public static boolean areArraysEqual(int[] arr1, int[] arr2) {
       /*
        * This method should return true if the two 
        * input arrays, arr1 and arr2, are equal.
        * First, think about what it means for two arrays to be equal.
        * There are two requirements:
-       *    1) Arrays have the same length.
-       *    2) Each corresponding element must be the same.
+       *    1) 
+       *    2) 
        * Then, think about which is easier to verify:
        *    - two arrays are equal
        *    - two arrays are *not* equal
        */
-       
-      // Your code goes below this line (suggested length: ~10 lines)
-      if (arr1.length != arr2.length) {
-         return false;
-      }
-      
-      for (int i = 0; i < arr1.length; i++) {
-         if (arr1[i] != arr2[i]) {
-            return false;
-         }
-      }
 
-      return true;
+      // Your code goes below this line (suggested length: ~10 lines)
+      return false;
+
    }
-   
+
    public static int[] findAndReplace(int[] arr, int findValue, int replaceValue) {
       /*
        * This method loops over the input array and overwrites 
@@ -97,17 +75,11 @@ public class TraversalTests {
        *
        * Question to think about: why might 2) be a better solution that 1)?  
        */
-       
-      // [2, 1, 8] --> [8, 1, 2]
-      // Your code goes below this line (suggested length: ~5 lines)
-      int[] reversed = new int[arr.length];
 
-      for (int i = 0; i < arr.length; i++) {
-         reversed[arr.length -1  - i] = arr[i];
-      }
-      return reversed;
+      // Your code goes below this line (suggested length: ~5 lines)
+      return arr;
    }
-   
+
    /*************************************************************************/
    /*                                                                       */
    /*                                                                       */
