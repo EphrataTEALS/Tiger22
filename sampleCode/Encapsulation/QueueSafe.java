a1cb6fb36c0b17fdf5beb8b16e576f02da017cb2
ankurdhoot <ankurdhoot97@gmail.com>
2022-02-24 05:04:23 +0000

    Add Encapsulation Lession

---
+++ sampleCode/Encapsulation/QueueSafe.java
@@ -0,0 +1,28 @@
+package sampleCode.Encapsulation;
+
+import java.util.ArrayList;
+
+public class QueueSafe {
+    private ArrayList<Integer> list;
+
+    public QueueSafe() {
+        list = new ArrayList<Integer>();
+    }
+
+    // TODO: Implement this method.
+    // Returns the oldest element in the queue.
+    // If there's nothing to remove, return -1.
+    public int dequeue() {
+        return -1;
+    }
+
+    // TODO: Implement this method
+    // Adds the element to the queue.
+    public void enqueue(int element) {
+        
+    }
+
+    public String toString() {
+        return list.toString();
+    }
+}
