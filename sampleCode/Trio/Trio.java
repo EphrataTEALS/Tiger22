df9f7de41aa6034a17f1eb058f07979133019aba
ankurdhoot <ankurdhoot97@gmail.com>
2022-03-31 15:57:17 +0000

    Add Trio implementation

--- sampleCode/Trio/Trio.java
+++ sampleCode/Trio/Trio.java
@@ -1,2 +1,26 @@
 public class Trio implements MenuItem {
+    private Sandwich sandwich;
+    private Salad salad;
+    private Drink drink;
+
+    public Trio(Sandwich sandwich, Salad salad, Drink drink) {
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
+    }
+
+    @Override
+    public String getName() {
+        return this.sandwich.getName() 
+        + '/' + this.salad.getName()
+        + '/' + this.drink.getName() 
+        + " Trio";
+    }
+
+    @Override
+    public double getPrice() {
+        return this.sandwich.getPrice() 
+        + this.salad.getPrice() 
+        + this.drink.getPrice();
+    }
 }
