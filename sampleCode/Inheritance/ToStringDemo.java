8e2b2a46358fa15bc775955ccdc4749fe8b66381
ankurdhoot <ankurdhoot97@gmail.com>
2022-03-31 05:15:51 +0000

    Add Trio Demo

--- sampleCode/Inheritance/ToStringDemo.java
+++ sampleCode/Inheritance/ToStringDemo.java
@@ -6,9 +6,14 @@ public class ToStringDemo {
         this.name = name;
     }
 
+    public String toString() {
+        return "ToStringDemo : " + name;
+    }
+
     public static void main(String[] args) {
         ToStringDemo bob = new ToStringDemo("bob");
         System.out.println(bob);
+        System.out.println(bob.toString());
 
         ToStringDemo alice = new ToStringDemo("alice");
         System.out.println(alice);
---
+++ sampleCode/Trio/Sandwich.java
@@ -0,0 +1,21 @@
+public class Sandwich implements MenuItem {
+    private String name;
+    private double price;
+
+    public Sandwich(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+
+}
